a9247ead726d9c1de2c54ebc32d208ea3de00c74
Carlos Duarte Rodríguez <130692626+CarlosDuarteRod@users.noreply.github.com>
2023-09-29 18:06:12 +0000

    Proyecto final

--- src/api/routes.py
+++ src/api/routes.py
@@ -7,11 +7,12 @@ from api.utils import generate_sitemap, APIException
 from flask_jwt_extended import create_access_token
 from flask_jwt_extended import get_jwt_identity
 from flask_jwt_extended import jwt_required
+from werkzeug.security import check_password_hash
 
 api = Blueprint('api', __name__)
 
 
-@api.route('/hello', methods=['POST', 'GET'])
+@api.route('/hello', methods=['GET'])
 def handle_hello():
 
     response_body = {
@@ -21,20 +22,49 @@ def handle_hello():
     return jsonify(response_body), 200
 
 
-@api.route('/login', methods=['POST'])
-def login():
-    data = request.json
-    user = User.query.filter_by(email = data["username"], password = data["password"]).first()
-    if user:
-        token = create_access_token(identity = user.id)
-        return jsonify ({"token": token})
-    return jsonify({"caco": "usuario o contraseña incorrecto"}), 200
-
-
-@api.route('/user', methods=['GET'])
-@jwt_required()
+@api.route('/users', methods=['GET'])
 def get_user():
-    user_id = get_jwt_identity()
-    user = User.query.filter_by(id = user_id).first()
+    users = db.session.execute(db.select(User).order_by(User.email)).scalars()
+    results = [user.serialize() for user in users]
+    response_body = {
+        "Message": "List of user register",
+        "users:": results
+    }
 
-    return jsonify(user.serialize())
+    return response_body, 200
+
+
+@api.route('/register', methods=['POST'])
+def create_user():
+    # 1. Obterner el contenido JSON de la solicitud POST entrante utilizando el objeto request. El método get_json()analiza y devuelve el contenido JSON de la solicitud
+    request_body = request.get_json() 
+    # 2. Crear instancia de la clase User. Los valores para las propiedades email y password se toman del request_body
+    user = User(email=request_body["email"], password=request_body["password"], is_active=True)
+    # 3. Agrega el objeto user a la base de datos
+    db.session.add(user)
+    # 4.Confirmar los cambios
+    db.session.commit()
+    
+    return ({
+        "message": "User registered successfully", 
+        'new user': request_body
+        }), 200
+
+
+@api.route('/token', methods=['POST'])
+def create_token():
+    email = request.json.get("email", None)
+    password = request.json.get("password", None)
+
+    if not email or not password:
+        return jsonify({"message": "Missing email or password"}), 400
+
+    # Busca al usuario en la base de datos
+    user = User.query.filter_by(email=email, password= password).first()  
+
+    if user: 
+        token = create_access_token(identity= user.id)
+        return jsonify({"token": token}), 200
+    
+    return ({"Error": "Bad user or password"}), 400
+
